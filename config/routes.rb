dfb21e0d0049dd7ebc82dc899696557dce2ea4af
Matthew The <matthew.the@tum.de>
2020-08-16 13:53:43 +0200

    Added about page, upgraded buttons

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,8 @@ Rails.application.routes.draw do
   
   get 'products/:id/merge' => 'products#merge', as: :merge_product
   
+  get "/pages/:page" => "pages#show"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
